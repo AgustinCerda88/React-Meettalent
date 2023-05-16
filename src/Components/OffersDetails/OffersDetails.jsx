24d0aa8c19e6edf608d26f8002341467bb3c6031
Haruuth <haruuth@gmail.com>
2023-05-16 10:44:19 +0200

    correccion y edicion del formulario

--- src/Components/OffersDetails/OffersDetails.jsx
+++ src/Components/OffersDetails/OffersDetails.jsx
@@ -6,10 +6,12 @@ import { IoIosArrowBack } from "react-icons/io";
 import { Link } from "react-router-dom";
 import "./OffersDetails.css";
 import { SearchContext } from "../../App";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
 
 export const OffersDetails = () => {
-  // const newOffer = useContext(SearchContext);
+  const [editMode, setEditMode] = useState(false);
+  const [editedOffer, setEditedOffer] = useState({});
+
   const { newOffer, setNewOffer } = useContext(SearchContext);
   const [offer, setOffer] = useState(null);
   const { id } = useParams();
@@ -32,17 +34,33 @@ export const OffersDetails = () => {
 
   const deleteOffer = async (id) => {
     try {
-      
       await axios.delete(`http://localhost:8000/offers/${id}`);
       setNewOffer(null);
       console.log("Oferta eliminada con exito");
       navigate("/home");
     } catch (error) {
-      alert("No se ha encontrado la oferta a eliminar. Tal vez ya haya sido eliminada.")
+      alert(
+        "No se ha encontrado la oferta a eliminar. Tal vez ya haya sido eliminada."
+      );
       console.error(error);
     }
   };
 
+  const handleEdit = () => {
+    setEditMode(true);
+    setEditedOffer({ ...offer });
+  };
+
+  const handleSave = async () => {
+    try {
+      await axios.put(`http://localhost:8000/offers/${offer._id}`, editedOffer);
+      setOffer(editedOffer);
+      setEditMode(false);
+      console.log("Oferta actualizada con éxito");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="page">
@@ -66,60 +84,201 @@ export const OffersDetails = () => {
                 <p>
                   <strong>Descripción:</strong>
                 </p>
-                <p> {offer.description}</p>
+                {editMode ? (
+                  <input
+                    type="text"
+                    value={editedOffer.description}
+                    onChange={(e) =>
+                      setEditedOffer({
+                        ...editedOffer,
+                        description: e.target.value,
+                      
+                      })
+                    }
+                  />
+                ) : (
+                  <p>{offer.description}</p>
+                )}
               </div>
 
               <div>
                 <p>
                   <strong>Requisitos:</strong>
                 </p>
-                <p> {offer.requirements}</p>
+                {editMode ? (
+                  <input
+                    type="text"
+                    value={editedOffer.requirements}
+                    onChange={(e) =>
+                      setEditedOffer({
+                        ...editedOffer,
+                        requirements: e.target.value,
+                      })
+                    }
+                  />
+                ) : (
+                  <p>{offer.requirements}</p>
+                )}
               </div>
 
               <div>
                 <p>
                   <strong>Palabras Clave:</strong>
                 </p>
-                <p> {offer.keywords}</p>
+                {editMode ? (
+                  <input
+                    type="text"
+                    value={editedOffer.keywords}
+                    onChange={(e) =>
+                      setEditedOffer({
+                        ...editedOffer,
+                        keywords: e.target.value,
+                      })
+                    }
+                  />
+                ) : (
+                  <p>{offer.keywords}</p>
+                )}
               </div>
 
               <div>
                 <p>
                   <strong>Salario:</strong>
                 </p>
-                <p> {offer.salary}</p>
+                {editMode ? (
+                  <input
+                    type="text"
+                    value={editedOffer.salary}
+                    onChange={(e) =>
+                      setEditedOffer({
+                        ...editedOffer,
+                        salary: e.target.value,
+                      })
+                    }
+                  />
+                ) : (
+                  <p>{offer.salary}</p>
+                )}
               </div>
 
               <div>
                 <p>
                   <strong>Ubicación:</strong>
                 </p>
-                <p> {offer.location}</p>
+                {editMode ? (
+                  <input
+                    type="text"
+                    value={editedOffer.location}
+                    onChange={(e) =>
+                      setEditedOffer({
+                        ...editedOffer,
+                        location: e.target.value,
+                      })
+                    }
+                  />
+                ) : (
+                  <p>{offer.location}</p>
+                )}
               </div>
 
               <div>
                 <p>
                   <strong>Tipo de Jornada:</strong>
                 </p>
-                <p> {offer.scheduleType}</p>
+                {editMode ? (
+                  <input
+                    type="text"
+                    value={editedOffer.scheduleType}
+                    onChange={(e) =>
+                      setEditedOffer({
+                        ...editedOffer,
+                        scheduleType: e.target.value,
+                      })
+                    }
+                  />
+                ) : (
+                  <p>{offer.scheduleType}</p>
+                )}
               </div>
 
               <div>
                 <p>
                   <strong>Tipo de Contrato:</strong>
                 </p>
-                <p> {offer.contractType}</p>
+                {editMode ? (
+                  <input
+                    type="text"
+                    value={editedOffer.contractType}
+                    onChange={(e) =>
+                      setEditedOffer({
+                        ...editedOffer,
+                        contractType: e.target.value,
+                      })
+                    }
+                  />
+                ) : (
+                  <p>{offer.contractType}</p>
+                )}
               </div>
 
               <div>
                 <p>
                   <strong>Disponibilidad</strong>
                 </p>
-                <p> {offer.availability}</p>
+                {editMode ? (
+                  <input
+                    type="text"
+                    value={editedOffer.availability}
+                    onChange={(e) =>
+                      setEditedOffer({
+                        ...editedOffer,
+                        availability: e.target.value,
+                      })
+                    }
+                  />
+                ) : (
+                  <p>{offer.availability}</p>
+                )}
               </div>
-              <button style={{height: "20px", width: "100px", backgroundcolor:"red"}} 
-              onClick={() => deleteOffer(offer._id)}
-              >Eliminar oferta</button>
+
+              {!editMode && (
+                <button
+                  style={{
+                    height: "20px",
+                    width: "100px",
+                    backgroundColor: "red",
+                  }}
+                  onClick={() => deleteOffer(offer._id)}
+                >
+                  Eliminar oferta
+                </button>
+              )}
+
+              {editMode && (
+                <button
+                  style={{
+                    height: "20px",
+                    width: "100px",
+                    backgroundColor: "green",
+                  }}
+                  onClick={handleSave}
+                >
+                  Guardar
+                </button>
+              )}
+
+              {!editMode && (
+                <button
+                  style={{
+                    height: "20px",
+                    width: "100px",
+                    backgroundColor: "blue",
+                  }}
+                  onClick={handleEdit}
+                >
+                  Editar
+                </button>
+              )}
             </div>
           ) : (
             <p>Cargando las ofertas...</p>
